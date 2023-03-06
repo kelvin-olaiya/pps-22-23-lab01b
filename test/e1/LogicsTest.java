7c5a743d1ff7214c5c0e39d8320e560a96a719dd
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-06 09:56:48 +0100

    chore: add basic test

--- test/e1/LogicsTest.java
+++ test/e1/LogicsTest.java
@@ -1,4 +1,47 @@
+package e1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LogicsTest {
-  
+
+
+    public static final int CHESSBOARD_SIZE = 9;
+    private Logics logics;
+
+    @BeforeEach
+    void setUp() {
+        this.logics = new LogicsImpl(CHESSBOARD_SIZE);
+    }
+
+    @Test
+    void testBoardHasOnlyOneKnight() {
+        assertEquals(1, countPiecesOnBoardWith(this.logics::hasKnight));
+    }
+
+    @Test
+    void testBoardHasOnlyOnePawn() {
+        assertEquals(1, countPiecesOnBoardWith(this.logics::hasPawn));
+    }
+
+    private int countPiecesOnBoardWith(BiPredicate<Integer, Integer> positionChecker) {
+        int counter = 0;
+        for (int i = 0; i < CHESSBOARD_SIZE; i++) {
+            for (int j = 0; j < CHESSBOARD_SIZE; j++) {
+                if (positionChecker.test(i, j)) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
 }
