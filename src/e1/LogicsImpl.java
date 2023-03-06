05ab3bb8d08878d03e601c03d3a8482cac16b711
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-07 00:08:51 +0100

    refactor: add a constructor

--- src/e1/LogicsImpl.java
+++ src/e1/LogicsImpl.java
@@ -14,6 +14,12 @@ public class LogicsImpl implements Logics {
         this.pawn = this.randomEmptyPosition();
         this.knight = this.randomEmptyPosition();	
     }
+
+	public LogicsImpl(int size, Pair<Integer, Integer> pawnPosition, Pair<Integer, Integer> knightPosition) {
+		this.size = size;
+		this.pawn = pawnPosition;
+		this.knight = knightPosition;
+	}
     
 	private final Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
