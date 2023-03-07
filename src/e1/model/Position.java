513403b7eb96fb8f20d86ae9c789df538457cfcc
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-07 18:58:14 +0100

    chore: add utility Position

--- src/e1/model/Position.java
+++ src/e1/model/Position.java
@@ -1,2 +1,9 @@
-package e1.model.pieces;public class Position {
+package e1.model;
+
+import e1.Pair;
+
+public class Position extends Pair<Integer, Integer> {
+    public Position(Integer x, Integer y) {
+        super(x, y);
+    }
 }
