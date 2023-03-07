5f8215d850c19a3b78263f177853ba3133086cf3
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-07 18:56:49 +0100

    chore: implement and test knigt

--- test/e1/model/pieces/ChessPieceTest.java
+++ test/e1/model/pieces/ChessPieceTest.java
@@ -1,23 +1,61 @@
-package e1.model;
+package e1.model.pieces;
 
-import e1.model.pieces.ChessPiece;
-import e1.model.pieces.StaticPawn;
+import e1.model.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ChessPieceTest {
 
     protected ChessPiece piece;
 
     static class TestStaticPawn extends ChessPieceTest {
+
+        private static final Position STARTING_POSITION = new Position(3, 4);
+        private static final List<Position> TEST_POSITIONS = List.of(
+                new Position(3, 5), new Position(3, 5), new Position(2, 6)
+        );
+
         @BeforeEach
         void setUp() {
             this.piece = new StaticPawn();
         }
 
         @Test
-        void canMoveToPositionFrom() {
+        void testCanMoveToPositionFrom() {
+            TEST_POSITIONS.forEach(position ->
+                    assertFalse(this.piece.canMoveToPositionFrom(position, STARTING_POSITION))
+            );
+        }
+    }
 
+    static class TestKnight extends ChessPieceTest {
+
+        private static final Position STARTING_POSITION = new Position(5, 5);
+        private static final List<Position> LEGAL_MOVES = List.of(
+                new Position(3, 6), new Position(3, 4),
+                new Position(6, 7), new Position(4, 7),
+                new Position(7, 4), new Position(7, 6),
+                new Position(6, 3), new Position(4, 3)
+        );
+        private static final List<Position> ILLEGAL_MOVES = List.of(
+                new Position(0, 0), new Position(5, 5), new Position(2, 3), new Position(4, 5)
+        );
+
+        @BeforeEach
+        void setUp() {
+            this.piece = new Knight();
+        }
+
+        @Test
+        void testCanMoveToLegalPositions() {
+                LEGAL_MOVES.forEach(position ->
+                        assertTrue(this.piece.canMoveToPositionFrom(position, STARTING_POSITION))
+                );
         }
     }
 
