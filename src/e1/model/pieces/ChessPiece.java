bc480dd778f3fd12273c9634753d40a954259ef9
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-09 09:08:11 +0100

    refactor: implement piece via strategy

--- src/e1/model/pieces/ChessPiece.java
+++ src/e1/model/pieces/ChessPiece.java
@@ -2,6 +2,7 @@ package e1.model.pieces;
 
 import e1.model.Position;
 
+@FunctionalInterface
 public interface ChessPiece {
     boolean canMoveToPositionFrom(Position to, Position from);
 }
--- src/e1/model/pieces/PieceFactoryImpl.java
+++ src/e1/model/pieces/PieceFactoryImpl.java
@@ -1,13 +1,30 @@
 package e1.model.pieces;
 
+import e1.model.Position;
+
 public class PieceFactoryImpl implements PieceFactory {
     @Override
     public ChessPiece newStaticPawn() {
-        return new StaticPawn();
+        return new ChessPiece() {
+            @Override
+            public boolean canMoveToPositionFrom(Position to, Position from) {
+                return false;
+            }
+        };
     }
 
     @Override
     public ChessPiece newKnight() {
-        return new Knight();
+        return new ChessPiece() {
+            private static final int KNIGHT_MAX_MOVE = 2;
+            private static final int KNIGHT_MIN_MOVE = 1;
+
+            @Override
+            public boolean canMoveToPositionFrom(Position to, Position from) {
+                int x = Math.abs(to.getX() - from.getX());
+                int y = Math.abs(to.getY() - from.getY());
+                return Math.min(x, y) != 0 && x + y == KNIGHT_MAX_MOVE + KNIGHT_MIN_MOVE;
+            }
+        };
     }
 }
